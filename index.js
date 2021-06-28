7166446adcef1bd47de15435867c88d886eb2e4a
Obi <mr.obiume@gmail.com>
2021-06-28 15:44:10 -0700

    commit tree 879966f38651ad8b0a0518a7200b62b8895093cf

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(task = `go to the office`) {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(wrap= `*`) {
+    function greeting(greet= `special`) {
+        return `You are ${wrap}${greet}${wrap}!`;
+    }
+    return greeting;
+}
